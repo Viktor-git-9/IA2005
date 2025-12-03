c5f7272e5bb3d61bba8831249f2aef5ecf1cee80
Viktor-git-9 <viktor.essbach@gmx.de>
2025-12-03 14:38:01 +0100

    Added a module with two subroutines, for writing real and complex arrays to .dat files. Will be useful for debugging.

---
+++ write_2Darray.f90
@@ -0,0 +1,64 @@
+! Subroutine write_2Darray_real.f90
+! Write the indices and elements of a real double precision 2D array to a .dat file.
+! Careful, this is slow for large arrays. Use for debugging.
+! Inputs: data (2D array of real double precision numbers), filename (character string to designate file)
+! Outputs: writes data to file
+! Author: Viktor Essbach (v.essbach@ens.psl.fr), 12/25
+
+Module writeArrays
+IMPLICIT NONE
+CONTAINS
+
+SUBROUTINE write_real_2DArray(data, filename)
+    ! Subroutine write_real_2Darray
+    ! Write the indices and elements of a real double precision 2D array to a .dat file.
+    ! Careful, this is slow for large arrays. Use for debugging.
+    ! Inputs: data (2D array of real double precision numbers), filename (character string to designate file)
+    ! Outputs: writes data to file
+    ! Author: Viktor Essbach (v.essbach@ens.psl.fr), 12/25
+    IMPLICIT NONE
+    INTEGER :: i, j, nRows, nCols
+    DOUBLE PRECISION, intent(in) :: data(:,:)
+    CHARACTER(len=*), intent(in) :: filename
+
+    nRows = size(data,1)
+    nCols = size(data,2)
+    open(19, file=filename)
+    do i = 1, nRows
+        do j = 1, nCols
+            write(19, 100) i, j, data(i,j)
+        enddo
+    enddo
+    close(19)
+    100 format(i5, 1x, i5, 1x, f15.6)
+
+END SUBROUTINE
+
+SUBROUTINE write_cmplx_2DArray(data, filename)
+    ! Subroutine write_cmplx_2Darray
+    ! Write the indices and elements of a complex double precision 2D array to a .dat file.
+    ! Careful, this is slow for large arrays. Use for debugging.
+    ! Inputs: data (2D array of complex double precision numbers), filename (character string to designate file)
+    ! Outputs: writes data to file
+    ! Author: Viktor Essbach (v.essbach@ens.psl.fr), 12/25
+    IMPLICIT NONE
+    INTEGER :: i, j, nRows, nCols
+    DOUBLE COMPLEX, intent(in) :: data(:,:)
+    CHARACTER(len=*), intent(in) :: filename
+
+    nRows = size(data,1)
+    nCols = size(data,2)
+    write(*,*) "Made it here"
+    write(*,*) data(1,1)
+    open(19, file=filename)
+    do i = 1, nRows
+        do j = 1, nCols
+            write(19, 100) i, j, real(data(i,j)), aimag(data(i,j))
+        enddo
+    enddo
+    close(19)
+    100 format(i5, 1x, i5, 1x, f15.6, 1x, f15.6)
+
+END SUBROUTINE
+
+END MODULE writeArrays
