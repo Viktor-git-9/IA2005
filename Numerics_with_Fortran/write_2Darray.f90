973504ae8c9404df218a1aee1f58834ac8bc9430
Viktor-git-9 <viktor.essbach@gmx.de>
2026-02-16 14:36:11 +0100

    Added subroutine to write real 1D array to .dat file.

--- Numerics_with_Fortran/write_2Darray.f90
+++ Numerics_with_Fortran/write_2Darray.f90
@@ -2,6 +2,29 @@ Module writeArrays
 IMPLICIT NONE
 CONTAINS
 
+SUBROUTINE write_real_1DArray(data, filename)
+    ! Subroutine write_real_1Darray
+    ! Write the indices and elements of a real double precision 1D array to a .dat file.
+    ! Careful, this is slow for large arrays. Use for debugging.
+    ! Inputs: data (1D array of real double precision numbers), filename (character string to designate file)
+    ! Outputs: writes data to file
+    ! Author: Viktor Essbach (v.essbach@ens.psl.fr), 02/26
+    IMPLICIT NONE
+    INTEGER :: i, nRows
+    DOUBLE PRECISION, intent(in) :: data(:)
+    CHARACTER(len=*), intent(in) :: filename
+    write(*,*) "Writing to file..."
+    nRows = size(data)
+    open(19, file=filename)
+    do i = 1, nRows
+        write(19, 100) i, data(i)
+    enddo
+    close(19)
+    100 format(i5, 1x, f15.6)
+    write(*,*) "File written!"
+
+END SUBROUTINE
+
 SUBROUTINE write_real_2DArray(data, filename)
     ! Subroutine write_real_2Darray
     ! Write the indices and elements of a real double precision 2D array to a .dat file.
